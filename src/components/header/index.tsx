f7243991da81ae62f840c24f41b9b3f272db3397
Stefano Mitello <stefano.mitello@gmail.com>
2022-03-10 18:09:01 +0100

    add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
+
+
+
+const Header = () => {
+    return (
+        <>
+            <Flex as='header' bg='tomato' w='100%' p={4} color='white' pos="fixed" zIndex={2}>
+                <Box p='2'>
+                    <Heading size='md'>Beer</Heading>
+                </Box>
+            </Flex>
+        </>
+    );
+}
+
+export default Header;
