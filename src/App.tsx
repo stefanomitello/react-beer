6d8838fb8cd01fb37dcdd8f3bc2498d45383a759
Stefano Mitello <stefano.mitello@gmail.com>
2022-03-10 18:35:49 +0100

    feat(footer): add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header';
 import './App.css';
+import Footer from './components/footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
