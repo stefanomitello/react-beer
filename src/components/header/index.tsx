42e86eeb7cb987d3e264d915ec0f059a9c054f9d
Stefano Mitello <stefano.mitello@gmail.com>
2022-03-11 20:28:28 +0100

    fix(header): added position param on flex element

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 
 const Header = () => {
     return (
-        <Flex as='header' bg='tomato' w='100%' p={4} color='white' pos="fixed" zIndex={2}>
+        <Flex as='header' bg='tomato' w='100%' p={4} color='white' pos="fixed" top="0" left="0" zIndex={2}>
             <Box p='2'>
                 <Heading size='md'>Beer</Heading>
             </Box>
